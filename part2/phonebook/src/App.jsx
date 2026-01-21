99bdb3fd952af966372b9ad73a9be0f324edfe27
Toki001 <jameskierdoliguez@gmail.com>
2026-01-21 15:50:14 +0800

    Exercise 2.13

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
+import personService from './service/persons'
 
 const Filter = ({ filter, handleFilter }) => <div>filter shown with<input value={filter} onChange={handleFilter}/></div>
 
@@ -27,10 +27,10 @@ const App = () => {
   const [filter, setFilter] = useState('')
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        setPersons(response.data)
+    personService
+      .getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons)
       })
   }, [])
 
@@ -54,13 +54,13 @@ const App = () => {
         number: newNumber,
         id: newName
       }
-      axios
-      .post('http://localhost:3001/persons', personObject)
-      .then(response => {
-        setPersons(persons.concat(response.data))
-        setNewName('')
-        setNewNumber('')
-      })
+      personService
+        .create(personObject)
+        .then(returnedPerson => {
+          setPersons(persons.concat(returnedPerson))
+          setNewName('')
+          setNewNumber('')
+        })
     }
   }
 
